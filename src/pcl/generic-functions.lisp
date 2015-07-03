5df862f2ebd384b26b6428956058778727f07a1c
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-07-03 23:19:32 +0200

    New generic function SPECIALIZER-TYPE-SPECIFIER
    The generic function returns a type specifier describing the objects
    accepted by a given (possibly non-parsed) specializer.
    
    This change is part of an effort to integrate the necessary extensions
    to support generalizer metaobjects [1] into SBCL's PCL.
    
    [1] https://github.com/sbcl/specializable

--- src/pcl/generic-functions.lisp
+++ src/pcl/generic-functions.lisp
@@ -356,6 +356,29 @@
 (defgeneric slot-unbound (class instance slot-name))
 
 (defgeneric slot-value-using-class (class object slotd))
+
+;; TODO maybe should not depend on method
+(defgeneric specializer-type-specifier
+    (proto-generic-function proto-method specializer)
+  #+sb-doc
+  (:documentation
+   "Return a type specifier for SPECIALIZER, a non-parsed specializer
+form or a SPECIALIZER instance.
+
+More specifically, SPECIALIZER can be
+* a non-parsed specializer form such as
+  * a symbol naming a class
+  * a list of the form (eql OBJECT)
+  * a list of the form (SPECIALIZER-KIND &rest SPECIFIC-SYNTAX)
+* an instance of a subclass of SPECIALIZER
+
+When SPECIALIZER cannot be parsed/used as a specializer for
+PROTO-GENERIC-FUNCTION and PROTO-METHOD, a STYLE-WARNING is signaled
+and NIL is returned. No type declaration will be generated in this
+case.
+
+NIL can also be returned if SPECIALIZER is valid but its type should
+not be declared, for example for efficiency reasons."))
 
 ;;;; 4 arguments
 
