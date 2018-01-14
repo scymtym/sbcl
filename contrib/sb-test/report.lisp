e98382ea441af96d739eb9e5a000527edac1d106
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-01-14 18:06:07 +0100

    sb-test: Report style protocol

--- contrib/sb-test/report.lisp
+++ contrib/sb-test/report.lisp
@@ -1,26 +1,62 @@
 (cl:in-package "TEST-UTIL")
 
-(defun report (all-failures &key report-skipped-tests (stream *standard-output*))
-  (terpri stream)
-  (format stream "Finished running tests.~%")
-  (let ((skipcount 0)
+(defun report (results &rest keys &key
+                                    (style :describe)
+                                    (target *standard-output*)
+                                    &allow-other-keys)
+  (let ((style (list* (case style
+                        (:describe 'style-describe)
+                        (t style))
+                      (loop for (key value) on keys by #'cddr
+                            unless (member key '(:style :target))
+                            collect key collect value))))
+   (report-using-style results style target)))
+
+(defgeneric report-using-style (results style target))
+
+(defmethod report-using-style ((results t) (style cons) (target t))
+  (let ((style (apply #'make-instance style)))
+    (report-using-style results style target)))
+
+(defmethod report-using-style ((results t) (style t) (target string))
+  (report-using-style results style (parse-namestring target)))
+
+(defmethod report-using-style ((results t) (style t) (target pathname))
+  (with-open-file (stream target :if-does-not-exist :create
+                          :direction :output
+                          :if-exists :supersede)
+    (report-using-style results style stream)))
+
+;;;; "describe" report style
+
+(defclass style-describe ()
+  ((report-skipped-tests :initarg  :report-skipped-tests
+                         :reader   report-skipped-tests
+                         :initform t)))
+
+(defmethod report-using-style
+    ((results t) (style style-describe) (target stream))
+  (terpri target)
+  (format target "Finished running tests.~%")
+  (let ((all-failures results)
+        (skipcount 0)
         (*print-pretty* nil))
     (cond (all-failures
-           (format stream "Status:~%")
+           (format target "Status:~%")
            (dolist (fail (reverse all-failures))
              (cond ((eq (car fail) :unhandled-error)
                     (output-colored-text (car fail)
                                          " Unhandled Error")
-                    (format stream " ~a~%"
+                    (format target " ~a~%"
                             (enough-namestring (second fail))))
                    ((eq (car fail) :invalid-exit-status)
                     (output-colored-text (car fail)
                                          " Invalid exit status:")
-                    (format stream " ~a~%"
+                    (format target " ~a~%"
                             (enough-namestring (second fail))))
                    ((eq (car fail) :skipped-disabled)
-                    (when report-skipped-tests
-                      (format stream " ~20a ~a / ~a~%"
+                    (when (report-skipped-tests style)
+                      (format target " ~20a ~a / ~a~%"
                               "Skipped (irrelevant):"
                               (enough-namestring (second fail))
                               (third fail)))
@@ -35,11 +71,11 @@
                        (:unexpected-success " Unexpected success:")
                        (:skipped-broken " Skipped (broken):")
                        (:skipped-disabled " Skipped (irrelevant):")))
-                    (format stream " ~a / ~a~%"
+                    (format target " ~a / ~a~%"
                             (enough-namestring (second fail))
                             (third fail)))))
            (when (> skipcount 0)
-             (format stream " (~a tests skipped for this combination of platform and features)~%"
+             (format target " (~a tests skipped for this combination of platform and features)~%"
                      skipcount)))
           (t
-           (format stream "All tests succeeded~%")))))
+           (format target "All tests succeeded~%")))))
